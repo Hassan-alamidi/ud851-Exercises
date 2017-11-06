7cabd813a6f3f32ce10f6df3fa5f0ac1da8e707d
Hassan Al-amidi <ha.alamidi@gmail.com>
2017-11-06 17:58:30 +0000

    exercise 1 and 2 complete

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -16,21 +16,31 @@
 package com.example.android.favoritetoys;
 
 import android.os.Bundle;
+import android.support.v4.widget.AutoSizeableTextView;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Declare a TextView variable called mToysListTextView
+    TextView mToysListTextView;
+    String[] toyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
 
-        // TODO (3) Use findViewById to get a reference to the TextView from the layout
+    }
 
-        // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+    @Override
+    protected void onStart(){
+      super.onStart();
+        toyList = ToyBox.getToyNames();
 
-        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        for(int i = 0; toyList.length > i; i++){
+            //should probably use scrollview to list items but since its not requested I will assume it will be requested in future exercises
+            mToysListTextView.append("\n" + toyList[i]);
+        }
     }
 }
--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/ToyBox.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/ToyBox.java
@@ -65,7 +65,8 @@ public final class ToyBox {
                 "Squirt Guns",
                 "Miniature Replica Animals Stuffed with Beads that you swore to your parents would be worth lots of money one day",
                 "Creepy Gremlin Doll",
-                "Neodymium-Magnet Toy"
+                "Neodymium-Magnet Toy",
+                "one Toy added"
         };
     }
 }
