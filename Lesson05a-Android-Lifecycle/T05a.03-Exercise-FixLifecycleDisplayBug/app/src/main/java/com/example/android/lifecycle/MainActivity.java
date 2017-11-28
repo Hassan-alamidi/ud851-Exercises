bba2a9187fd5697a02ff57883d2866f3272a886b
Hassan Al-amidi <ha.alamidi@gmail.com>
2017-11-28 13:06:08 +0000

    Lesson 5a and 5b complete

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
 
+    private static List<String> mLifecycleCallbacks = new ArrayList();
     /**
      * Called when the activity is first created. This is where you should do all of your normal
      * static set up: create views, bind data to lists, etc.
@@ -71,10 +74,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
 
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
-
+        if(mLifecycleCallbacks != null && mLifecycleCallbacks.size() > 0) {
+            for (int i = mLifecycleCallbacks.size() - 1; i >= 0; i--) {
+                String item = mLifecycleCallbacks.get(i);
+                mLifecycleDisplay.append(item + "\n");
+            }
+            mLifecycleCallbacks.clear();
+        }
         logAndAppend(ON_CREATE);
     }
 
@@ -132,9 +139,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
-
+        mLifecycleCallbacks.add(0,ON_STOP);
         logAndAppend(ON_STOP);
     }
 
@@ -159,9 +164,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
-
+        mLifecycleCallbacks.add(0,ON_DESTROY);
         logAndAppend(ON_DESTROY);
     }
 
--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -32,10 +32,9 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a static final key to store the query's URL
 
-    // TODO (2) Create a static final key to store the search's raw JSON
-
+    private static final String queryUrl = "query";
+    private static final String rawJson = "results";
     private EditText mSearchBoxEditText;
 
     private TextView mUrlDisplayTextView;
@@ -59,7 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
-        // TODO (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        if(savedInstanceState != null){
+            String urlText = savedInstanceState.getString(queryUrl);
+            String searchResults = savedInstanceState.getString(rawJson);
+            mUrlDisplayTextView.setText(urlText);
+            mSearchResultsTextView.setText(searchResults);
+
+
+        }
     }
 
     /**
@@ -151,13 +157,14 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (3) Override onSaveInstanceState to persist data across Activity recreation
     // Do the following steps within onSaveInstanceState
-    // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
+    @Override
+    public void onSaveInstanceState(Bundle b){
+        super.onSaveInstanceState(b);
+        String url = mUrlDisplayTextView.getText().toString();
+        b.putString(queryUrl,url);
+        String jsonRaw = mSearchResultsTextView.getText().toString();
+        b.putString(rawJson,jsonRaw);
+    }
 
-    // TODO (5) Put the contents of the TextView that contains our URL into a variable
-    // TODO (6) Using the key for the query URL, put the string in the outState Bundle
-
-    // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
-    // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
 }
